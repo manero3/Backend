80340ad13223becb02fcaa436902c8afb2d9417e
madickeneriksson <conlerc@live.se>
2023-11-09 11:41:48 +0100

    Add project files.

---
+++ ManeroBackend/Contexts/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ManeroBackend.Contexts
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext()
+        {
+                
+        }
+
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
