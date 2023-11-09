78b877edad10cbcfbe1dc11c61cae89d540d6152
madickeneriksson <conlerc@live.se>
2023-11-09 12:19:49 +0100

    dtos added

---
+++ ManeroBackend/Models/DTOs/TokenRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace ManeroBackend.Models.DTOs
+{
+    public class TokenRequest
+    {
+        public string Code { get; set; }
+    }
+}
---
+++ ManeroBackend/Models/DTOs/UserRegistrationDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManeroBackend.Models.DTOs
+{
+    public class UserRegistrationDto
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid email address format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password must match")]
+        public string ConfirmPassword { get; set; }
+
+        public string? OAuthId { get; set; }
+        public string? OAuthProvider { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+    }
+}
