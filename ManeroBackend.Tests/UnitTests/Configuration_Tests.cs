dbec5a73ca3c6618c6d2f2e07a5398a31d3a083a
madickeneriksson <conlerc@live.se>
2023-11-10 10:24:28 +0100

    configuration test

---
+++ ManeroBackend.Tests/UnitTests/Configuration_Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace ManeroBackend.Tests.UnitTests;
+
+public class Configuration_Tests
+{
+    [Fact]
+    public void Can_Read_Google_Authentication_Settings()
+    {
+        // Arrange
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json") 
+            .Build();
+
+        // Act
+        var clientId = configuration["Authentication:Google:ClientId"];
+        var clientSecret = configuration["Authentication:Google:ClientSecret"];
+
+        // Assert
+        Assert.NotNull(clientId);
+        Assert.NotNull(clientSecret);
+    }
+}
